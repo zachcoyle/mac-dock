f0c664583480b63def096e7fbd4728130db0e1df
Zach Coyle <zach.coyle@gmail.com>
2021-06-07 22:09:58 -0400

    remove reference to ds_store file

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 import setuptools
 
 setuptools.setup(
-    name='mac-dock',
-    version='2020.12.3',
-    install_requires=open('requirements.txt').read().splitlines(),
+    name="mac-dock",
+    version="2020.12.3",
+    install_requires=open("requirements.txt").read().splitlines(),
     packages=setuptools.find_packages(),
-    scripts=['scripts/.DS_Store','scripts/dock','scripts/dock-add-app','scripts/dock-add-folder','scripts/dock-apps','scripts/dock-folders','scripts/dock-rm']
+    scripts=[
+        "scripts/dock",
+        "scripts/dock-add-app",
+        "scripts/dock-add-folder",
+        "scripts/dock-apps",
+        "scripts/dock-folders",
+        "scripts/dock-rm",
+    ],
 )
